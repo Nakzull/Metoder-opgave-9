9be2b1db903a169e3ea72c6f9e695ac15244ab72
Nakzull <andr25f4@zbc.dk>
2021-08-13 19:49:57 +0200

    Add project files.

---
+++ Metoder opgave 9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Metoder_opgave_9
+{
+
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            int[] userNumbers = new int[7];
+            while (true)
+            {
+                InsertNumbers(userNumbers);
+                int sammeTal = 0;
+                Random random = new Random();
+                List<int> lottoVinderTal = new List<int>();
+                for (int i = 0; i < 7; i++)
+                {
+                    lottoVinderTal.Add(random.Next(1, 21));
+                }
+                for (int i = 0; i < 7; i++)
+                {
+                    if (userNumbers[i] == lottoVinderTal[i])
+                        sammeTal++;
+                }
+                if (sammeTal >= 2)
+                    Console.WriteLine("Congratz you've won!");
+                else
+                    Console.WriteLine("You didn't win this time.. Better luck next time!");
+            }
+        }
+        public static int[] InsertNumbers(int[] a)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("Number " + (i + 1));
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            return a;
+        }
+    }
+}
